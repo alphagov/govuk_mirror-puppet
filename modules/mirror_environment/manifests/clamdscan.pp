9f487460f0222c0376f33dfa3bff0ce1912ddb82
bob walker <bob@randomness.org.uk>
2013-09-14 16:14:02 +0100

    [#56945538] Make the cron job a script.
    The script calls clamdscan such that it moves infected files to /srv/infected so make sure that exists.
    
    The script makes clamdscan log and adds a useful timestamp.

--- modules/mirror_environment/manifests/clamdscan.pp
+++ modules/mirror_environment/manifests/clamdscan.pp
@@ -9,8 +9,19 @@ class mirror_environment::clamdscan {
     content => '/srv/** r,',
     }
 
+  file { '/srv/infected':
+    ensure => directory,
+    }
+
+  file { '/usr/local/sbin/scan-srv-mirror_data.sh':
+    source => 'puppet:///modules/mirror_environment/usr/local/sbin/scan-srv-mirror_data.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    }
+
   cron { 'clamdscan-srv-mirrordata':
-    command => '/usr/bin/clamdscan /srv/mirror_data/',
-    minute  => fqdn_rand(60),
+    command     => '/usr/local/sbin/scan-srv-mirror_data.sh',
+    minute      => fqdn_rand(60),
     }
 }
