629c1d30524a05ba281b4fcd2ff7783ad2e61999
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-09-17 15:44:48 +0100

    [#56945538] add some requires to make sure things exist.

--- modules/mirror_environment/manifests/clamdscan.pp
+++ modules/mirror_environment/manifests/clamdscan.pp
@@ -8,6 +8,7 @@ class mirror_environment::clamdscan {
   apparmor::profile { 'usr.sbin.clamd':
     local_only   => true,
     local_source => 'puppet:///modules/mirror_environment/etc/apparmor.d/local/usr.sbin.clamd',
+    require      => Class['clamav'],
     }
 
   file { '/srv/infected':
@@ -15,14 +16,16 @@ class mirror_environment::clamdscan {
     }
 
   file { '/usr/local/sbin/scan-srv-mirror_data.sh':
-    source => 'puppet:///modules/mirror_environment/usr/local/sbin/scan-srv-mirror_data.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+    source  => 'puppet:///modules/mirror_environment/usr/local/sbin/scan-srv-mirror_data.sh',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Class['clamav'],
     }
 
   cron { 'clamdscan-srv-mirrordata':
     command     => '/usr/local/sbin/scan-srv-mirror_data.sh',
     minute      => fqdn_rand(60),
+    require     => File['/usr/local/sbin/scan-srv-mirror_data.sh'],
     }
 }
