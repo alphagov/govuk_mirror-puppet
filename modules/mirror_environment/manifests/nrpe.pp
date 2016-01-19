072fde8a6f830f224fd36486cf61a8192834e422
Matt Bostock <matt@mattbostock.com>
2016-01-19 09:53:52 +0000

    Set the mirror data mountpoint as class parameter

--- modules/mirror_environment/manifests/nrpe.pp
+++ modules/mirror_environment/manifests/nrpe.pp
@@ -2,8 +2,16 @@
 #
 # This class defines NRPE commands to be used with the pdxcat/nrpe module
 # this repository uses.
+#
+# === Parameters
+#
+# [*mirror_data_mountpoint*]
+#   The mount point for the data that the mirrors serve.
+#
 
-class mirror_environment::nrpe {
+class mirror_environment::nrpe (
+  $mirror_data_mountpoint,
+){
 
   nrpe::command {'check_disk':
     ensure  => present,
@@ -12,7 +20,7 @@ class mirror_environment::nrpe {
 
   nrpe::command {'check_disk_mirror-data':
     ensure  => present,
-    command => 'check_disk -w 10% -c 5% /srv/mirror_data',
+    command => "check_disk -w 10% -c 5% ${mirror_data_mountpoint}",
   }
 
 }
